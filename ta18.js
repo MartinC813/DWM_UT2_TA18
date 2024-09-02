377f3bb42f98bf6e09ade1d073f6232cde39e124
Martín <martin.cabral@correo.ucu.edu.uy>
2024-09-02 08:52:30 -0300

    Ta18

---
+++ ta18.js
@@ -0,0 +1,9 @@
+const listaTemplate= `
+                    <ul id="Personas">
+                        <li>Televisión</li>
+                        <li>Computadora</li>
+                        <li>Celular</li>
+                    </ul>
+                    `
+const div = document.getElementById("paraLaLista");
+div.innerHTML = listaTemplate;
